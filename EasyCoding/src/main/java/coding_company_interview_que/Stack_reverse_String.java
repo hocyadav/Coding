253ae6536330253e329adeb7e7268ae3c91537a9
Yadav, Hariom <Hariom.Yadav@ca.com>
2019-11-14 20:16:12 +0530

    reverse string

---
+++ EasyCoding/src/main/java/coding_company_interview_que/Stack_reverse_String.java
@@ -0,0 +1,33 @@
+package coding_company_interview_que;
+import java.util.*;
+/**
+ * 
+ * @author Hariom Yadav - Nov 4, 2019
+ *
+ */
+//ds
+//stack using java collection 
+
+//test ds
+public class Stack_reverse_String {
+	public static void main(String[] arg) {
+		StringBuffer sb = new StringBuffer("HariomYadav");
+		System.out.println(sb);
+		reverse(sb);
+		System.out.println(sb);
+		
+	}
+
+	private static void reverse(StringBuffer str) {
+		//push all char in stack
+		Stack<Character> st = new Stack<Character>();
+		for(int i=0; i<str.length(); i++) {
+			st.push(str.charAt(i));
+		}
+		
+		//pull all char from stack
+		for(int j=0; j<str.length(); j++) {
+			str.setCharAt(j, st.pop());
+		}
+	}
+}
